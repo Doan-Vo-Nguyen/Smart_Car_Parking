abd30adfd8a95774fe5dcda8d17746ef2f7712b5
Doan Vo Nguyen <wwwdoanvonguyen@gmail.com>
2024-11-20 08:31:21 +0700

    modified: fix logic for view vehicle and details veh, config port for db

--- vpms/admin/includes/dbconnection.php
+++ vpms/admin/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "car_parking");
+$con=mysqli_connect("localhost", "root", "", "car_parking", port:3307);
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
--- vpms/users/view--detail.php
+++ vpms/users/view--detail.php
@@ -2,46 +2,48 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['vpmsuid']==0)) {
-  header('location:logout.php');
-  } else{
+if (strlen($_SESSION['vpmsuid'] == 0)) {
+    header('location:logout.php');
+} else {
 
 
 
-  ?>
-<!doctype html>
+?>
+    <!doctype html>
 
-<html class="no-js" lang="">
-<head>
-   
-    <title>VPMS - View Vehicle Detail</title>
-   
+    <html class="no-js" lang="">
 
-    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+    <head>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
-    <link rel="stylesheet" href="../admin/assets/css/cs-skin-elastic.css">
-    <link rel="stylesheet" href="../admin/assets/css/style.css">
+        <title>VPMS - View Vehicle Detail</title>
 
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
-</head>
-<body>
-    <!-- Left Panel -->
+        <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
+        <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
 
-  <?php include_once('includes/sidebar.php');?>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+        <link rel="stylesheet" href="../admin/assets/css/cs-skin-elastic.css">
+        <link rel="stylesheet" href="../admin/assets/css/style.css">
 
-    <!-- Left Panel -->
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
-    <!-- Right Panel -->
+    </head>
 
-     <?php include_once('includes/header.php');?>
+    <body>
+        <!-- Left Panel -->
+
+        <?php include_once('includes/sidebar.php'); ?>
+
+        <!-- Left Panel -->
+
+        <!-- Right Panel -->
+
+        <?php include_once('includes/header.php'); ?>
 
         <div class="breadcrumbs">
             <div class="breadcrumbs-inner">
@@ -71,109 +73,115 @@ if (strlen($_SESSION['vpmsuid']==0)) {
         <div class="content">
             <div class="animated fadeIn">
                 <div class="row">
-                   
-         
 
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <strong class="card-title">View Vehicle details</strong>
+
+
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <strong class="card-title">View Vehicle details</strong>
+                            </div>
+                            <div class="card-body">
+                                <?php
+                                $cid = $_GET['viewid'];
+                                echo $cid;
+                                $ret = mysqli_query($con, "SELECT tbvehlogs.ParkingNumber, tbcat.VehicleCat, tbvehlogs.RegistrationNumber,
+                                tbuser.FullName,tbuser.MobileNumber,tbvehlogs.InTime,tbvehlogs.OutTime,tbvehlogs.Status,tbvehlogs.Remark,tbvehlogs.ParkingCharge
+                                FROM tblvehiclelogs as tbvehlogs
+                                JOIN tblvehicle as tbveh ON tbvehlogs.VehicleID=tbveh.ID
+                                JOIN tblregusers as tbuser ON tbuser.ID=tbveh.OwnerID
+                                JOIN tblcategory as tbcat ON tbcat.ID=tbveh.CategoryID
+                                WHERE tbvehlogs.ID='$cid'");
+                                $cnt = 1;
+                                while ($row = mysqli_fetch_array($ret)) {
+
+                                ?> <table border="1" class="table table-bordered mg-b-0">
+
+                                        <tr>
+                                            <th>Parking Number</th>
+                                            <td><?php echo $row['ParkingNumber']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Vehicle Category</th>
+                                            <td><?php echo $row['VehicleCat']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Registration Number</th>
+                                            <td><?php echo $row['RegistrationNumber']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Owner Name</th>
+                                            <td><?php echo $row['FullName']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Owner Contact Number</th>
+                                            <td><?php echo $row['MobileNumber']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>In Time</th>
+                                            <td><?php echo $row['InTime']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Status</th>
+                                            <td> <?php
+                                                    if ($row['Status'] == "In") {
+                                                        echo "Vehicle In";
+                                                    }
+                                                    if ($row['Status'] == "Out") {
+                                                        echo "Vehicle out";
+                                                    }; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Out Time</th>
+                                            <td> <?php
+                                                    if ($row['Status'] == "Out") {
+                                                       echo $row['OutTime'];
+                                                    }?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Remark</th>
+                                            <td><?php echo $row['Remark']; ?></td>
+                                        </tr>
+                                        <tr>
+                                        <tr>
+                                            <th>Parking Fee</th>
+                                            <td><?php echo $row['ParkingCharge']; ?></td>
+                                        </tr>
+
+                                    </table><?php } ?>
+
+                            </div>
                         </div>
-                        <div class="card-body">
-                  
-              <?php
- $cid=$_GET['viewid'];
-$ret=mysqli_query($con,"select * from tblvehicle where ID='$cid'");
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
 
-?>                       <table border="1" class="table table-bordered mg-b-0">
-   
-   <tr>
-                                <th>Parking Number</th>
-                                   <td><?php  echo $row['ParkingNumber'];?></td>
-                                   </tr>                             
-<tr>
-                                <th>Vehicle Category</th>
-                                   <td><?php  echo $row['VehicleCategory'];?></td>
-                                   </tr>
-                                   <tr>
-                                <th>Vehicle Company Name</th>
-                                   <td><?php  echo $packprice= $row['VehicleCompanyname'];?></td>
-                                   </tr>
-                                <tr>
-                                <th>Registration Number</th>
-                                   <td><?php  echo $row['RegistrationNumber'];?></td>
-                                   </tr>
-                                   <tr>
-                                    <th>Owner Name</th>
-                                      <td><?php  echo $row['OwnerName'];?></td>
-                                  </tr>
-                                      <tr>  
-                                       <th>Owner Contact Number</th>
-                                        <td><?php  echo $row['OwnerContactNumber'];?></td>
-                                    </tr>
-                                    <tr>
-                               <th>In Time</th>
-                                <td><?php  echo $row['InTime'];?></td>
-                            </tr>
-                            <tr>
-    <th>Status</th>
-    <td> <?php  
-if($row['Status']=="")
-{
-  echo "Vehicle In";
-}
-if($row['Status']=="Out")
-{
-  echo "Vehicle out";
-}
 
-     ;?></td>
-  </tr>
-   <tr>
-    <th>Remark</th>
-    <td><?php echo $row['Remark']; ?></td>
-  </tr>
-<tr>
-   <tr>
-    <th>Parking Fee</th>
-   <td><?php echo $row['ParkingCharge']; ?></td>
-  </tr>
 
-</table><?php } ?>
+
 
                     </div>
+
+
+
                 </div>
-                
+            </div><!-- .animated -->
+        </div><!-- .content -->
 
-  
+        <div class="clearfix"></div>
 
+        <?php include_once('includes/footer.php'); ?>
 
-            </div>
+        </div><!-- /#right-panel -->
 
+        <!-- Right Panel -->
 
-
-        </div>
-    </div><!-- .animated -->
-</div><!-- .content -->
-
-<div class="clearfix"></div>
-
-<?php include_once('includes/footer.php');?>
-
-</div><!-- /#right-panel -->
-
-<!-- Right Panel -->
-
-<!-- Scripts -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-<script src="../admin/assets/js/main.js"></script>
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+        <script src="../admin/assets/js/main.js"></script>
 
 
-</body>
-</html>
+    </body>
+
+    </html>
 <?php }  ?>
